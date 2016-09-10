36f44ed96f53b651949ea0a984866a7b1be44c9b
Sacha <sachabest@outlook.com>
2016-09-09 20:29:26 -0400

    fix compiler error in unreal code

--- Source/PennApps2016F/Private/AmazonRESTClient.cpp
+++ Source/PennApps2016F/Private/AmazonRESTClient.cpp
@@ -7,12 +7,4 @@ FString UAmazonRESTClient::AMAZON_URL = TEXT("https://amazon.com");
 
 TSharedPtr<TArray *> UAmazonRESTClient::searchItems(const FString *search)
 {
-	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
-	Request->OnProcessRequestComplete().BindUObject(this, &UAmazonRESTClient::OnResponseReceived);
-	//This is the url on which to process the request
-	Request->SetURL(UAmazonRESTClient::AMAZON_URL);
-	Request->SetVerb("GET");
-	Request->SetHeader(TEXT("User-Agent"), "X-UnrealEngine-Agent");
-	Request->SetHeader("Content-Type", TEXT("application/xml"));
-	Request->ProcessRequest();
 }
