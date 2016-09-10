47d3cbfb89ef014979bfb072b58a58502417b3f5
Akshay Shah <shahak@seas.upenn.edu>
2016-09-10 13:01:53 -0400

    added test alexa game

--- alexa-server/app.py
+++ alexa-server/app.py
@@ -1,8 +1,16 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+import logging
+from random import randint
+
+from flask_ask import Ask, statement, question, session
 from unreal_socket import UnrealSocket
 import sys
 
 app = Flask(__name__)
+
+ask = Ask(app, "/")
+
+logging.getLogger("flask_ask").setLevel(logging.DEBUG)
 
 @app.route('/')
 def hello_world():
@@ -27,3 +35,27 @@ def execute_command():
     params = request.json['params']
     # Step 4
     UnrealSocket.active_socket.process_command(command_name, params)
+
+@ask.launch
+def new_game():
+    welcome_msg = render_template('welcome')
+    return question(welcome_msg)
+
+
+@ask.intent("YesIntent")
+def next_round():
+    numbers = [randint(0, 9) for _ in range(3)]
+    round_msg = render_template('round', numbers=numbers)
+    session.attributes['numbers'] = numbers[::-1]  # reverse
+    return question(round_msg)
+
+
+@ask.intent("AnswerIntent", convert={'first': int, 'second': int, 'third': int})
+def answer(first, second, third):
+    print 'seesion attributes: ',session.attributes
+    winning_numbers = session.attributes['numbers']
+    if [first, second, third] == winning_numbers:
+        msg = render_template('win')
+    else:
+        msg = render_template('lose')
+    return statement(msg)
