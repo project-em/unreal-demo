d58d02150f1011233b2b260112ffc985de17be86
Sacha Best <sachabest@outlook.com>
2016-09-10 23:50:21 -0400

    change port

--- alexa-server/app.py
+++ alexa-server/app.py
@@ -126,4 +126,4 @@ def shutdown():
 
 if __name__ == '__main__':
     WSGIRequestHandler.protocol_version = "HTTP/1.1"
-    app.run(debug=True, threaded=True)
+    app.run(debug=True, threaded=True, port=443)
